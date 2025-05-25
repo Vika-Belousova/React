7f0b02e5a190ab2482e4635ea6edba66d17728a3
Vika-Belousova <91773388+Vika-Belousova@users.noreply.github.com>
2025-05-25 15:03:25 +0300

    Лабораторная работа 3. Node.js /Обработка ошибок,Создание middleware, Работа с файлами и регистрация пользователя,Создадим в папке middleware файл upload.js, в котором будет представлена работа с загрузкой файлов через multer

--- server/controllers/offerController.js
+++ server/controllers/offerController.js
@@ -1,10 +1,94 @@
+import { adaptOfferToClient } from '../adapters/offerAdapter.js';
+import { adaptFullOfferToClient } from '../adapters/offerAdapter.js';
 import {Offer} from '../models/offer.js'
+import { User } from '../models/user.js';
 async function getAllOffers(req, res, next) {
   try{
     const offers = await Offer.findAll(); 
-    res.send(offers);
+    const adaptedOffers = offers.map(adaptOfferToClient)
+    res.status(200).json(adaptedOffers);
   }catch(error){
     console.log('Не получилось получить список предложений: ', error)
   }
 }
-export {getAllOffers};
+async function getFullOffer(req, res, next) {
+  try {
+    const { id } = req.params;
+
+    const offer = await Offer.findByPk(id, {
+      include: { model: User, as: 'author' },
+    });
+
+    if (!offer) {
+      return next(ApiError.badRequest('Offer not found'));
+    }
+
+    const adapted = adaptFullOfferToClient(offer, offer.author);
+    res.status(200).json(adapted);
+  } catch (error) {
+    console.log('Не получилось получить предложение: ', error);
+    next(error);
+  }
+}
+export async function createOffer(req, res, next) {
+ try {
+   const {
+     title, description, publishDate, city,
+     isPremium, isFavorite, rating, type, rooms, guests, price,
+     features, commentsCount, latitude, longitude, userId
+   } = req.body;
+
+
+   if (!req.files?.previewImage || req.files.previewImage.length === 0) {
+     return next(ApiError.badRequest('Превью изображение обязательно для загрузки'));
+   }
+
+
+   const previewImagePath = `/static/${req.files.previewImage[0].filename}`;
+
+
+   let processedPhotos = [];
+   if (req.files?.photos) {
+     processedPhotos = req.files.photos.map(file => `/static/${file.filename}`);
+   }
+
+
+   let parsedFeatures = [];
+   if (features) {
+     try {
+       parsedFeatures = typeof features === 'string' ? JSON.parse(features) : features;
+     } catch {
+       parsedFeatures = features.split(',');
+     }
+   }
+
+
+   const offer = await Offer.create({
+     title,
+     description,
+     publishDate,
+     city,
+     previewImage: previewImagePath,
+     photos: processedPhotos,
+     isPremium,
+     isFavorite,
+     rating,
+     type,
+     rooms,
+     guests,
+     price,
+     features: parsedFeatures,
+     commentsCount,
+     latitude,
+     longitude,
+     authorId: userId
+   });
+   
+
+   return res.status(201).json(offer);
+ } catch (error) {
+   next(ApiError.internal('Не удалось добавить предложение: ' + error.message));
+ }
+}
+
+export {getAllOffers, getFullOffer };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import offerRoutes from'./offerRoutes.js';
+import userRotes from './userRoutes.js';
+import { use } from "react";
+
 const router = new Router();
 router.use('/', offerRoutes);
-export {router};
+router.use('/', userRotes);
+//export {router};
 export default router;
